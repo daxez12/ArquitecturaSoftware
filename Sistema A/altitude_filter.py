40652a0a521648cc7a8a8abe4b4b9141259d6629
Sergio Salazar <sesaez.23@gmail.com>
2015-03-16 19:03:52 -0600

    Cambios menores

--- Sistema A/altitude_filter.py
+++ Sistema A/altitude_filter.py
@@ -4,21 +4,19 @@ class AltitudeFilter(FilterFramework):
     def run(self):
         read = 0
         written = 0
-        id = 0
-        altitud = 0
+        data = 0        
         
-        
-        print"{0}::AltitudeFilter Reading ".format(self.getName())
+        print "{0}::AltitudeFilter Reading ".format(self.getName())
         
         while(True):
             try:
                 data = self.readFilterInputPort()
                 read += 1
-                self.writeFilterOutput(data)
+                self.writeFilterOutputPort(data)
                 written += 1
             
             except EndOfStreamException:
                 self.closePorts()
-                print "{0}::Altitude Filter Exiting; bytes read: {1}, bytes written: {2}".format(self.getName(), read, written)
+                #print "{0}::Altitude Filter Exiting; bytes read: {1}, bytes written: {2}".format(self.getName(), read, written)
                 break
                 
